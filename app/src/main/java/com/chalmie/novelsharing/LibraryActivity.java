f3825ea641286f0186a4cc636828dc2ac1f154a9
chalmie <blairchalmers@gmail.com>
2016-08-02 19:19:10 -0700

    add butterknife and click listeners

--- app/src/main/java/com/chalmie/novelsharing/LibraryActivity.java
+++ app/src/main/java/com/chalmie/novelsharing/LibraryActivity.java
@@ -5,29 +5,52 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class LibraryActivity extends AppCompatActivity {
-    private EditText mAddBookEditText;
-    private Button mAddBookButton;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class LibraryActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.addBookEditText) TextView mAddBookEditText;
+    @Bind(R.id.listView) ListView mListView;
+    @Bind(R.id.addBookButton) Button mAddBookButton;
+    private String[] books = new String[] {"Infinite Jest", "The Golden Compass",
+            "Of Mice and Men", "The Sun Also Rises", "Oliver Twist", "A Tale of Two Cities",
+            "Gravity's Rainbow", "Have Space Suit Will Travel", "The Client", "The Left Hand of Darkness",
+            "I, Robot", "Do Androids Dream of Electric Sheep", "1984",
+            "Brave New World", "We"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
+        ButterKnife.bind(this);
 
-        mAddBookEditText = (EditText) findViewById(R.id.addBookEditText);
-        mAddBookButton = (Button) findViewById(R.id.addBookButton);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, books);
+        mListView.setAdapter(adapter);
 
-        mAddBookButton.setOnClickListener(new View.OnClickListener() {
-
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-                String book = mAddBookEditText.getText().toString();
-                Intent intent = new Intent(LibraryActivity.this, AddBookActivity.class);
-                startActivity(intent);
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String book = ((TextView)view).getText().toString();
+                Toast.makeText(LibraryActivity.this, book, Toast.LENGTH_LONG).show();
             }
         });
+
+        mAddBookButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String book = mAddBookEditText.getText().toString();
+        Intent intent = new Intent(LibraryActivity.this, AddBookActivity.class);
+        intent.putExtra("book", book);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/chalmie/novelsharing/MainActivity.java
+++ app/src/main/java/com/chalmie/novelsharing/MainActivity.java
@@ -1,27 +1,35 @@
 package com.chalmie.novelsharing;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    private Button mLibraryButton;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.libraryButton) Button mLibraryButton;
+    @Bind(R.id.appNameTextView) TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        mLibraryButton = (Button) findViewById(R.id.libraryButton);
-        mLibraryButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LibraryActivity.class);
-                startActivity(intent);
-            }
-        });
+        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrich-regular.ttf");
+        mAppNameTextView.setTypeface(ostrichFont);
+
+        mLibraryButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(MainActivity.this, LibraryActivity.class);
+        startActivity(intent);
     }
 }
